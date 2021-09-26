a65de8fe3fd008119b09e5796f10b9681dd8746c
unknown <mdshafim953@gmail.com>
2021-09-26 16:09:47 +0600

    cart is updated

--- src/component/cart/cart.js
+++ src/component/cart/cart.js
@@ -12,15 +12,21 @@ const cart = (props) => {
     return (
 
         // Selected Singer Area 
-        <div className="position-fixed text-white mt-3">
+        <div className="sticky-top text-white mt-3">
             <div className="card bg-success">
                 <div className="card-header">
                     <h4 className="text-center">Selected Singers</h4>
                 </div>
                 <div className="p-4 ">
                     <h5>Singer Added: {props.cart.length}</h5>
-                    <h6 className="my-4">Total Salary Cost: {total}</h6>
-                    <h6>Singer Name: </h6>
+                    <h5 className="my-4">Total Salary: {total}</h5>
+                    <h5 className="mb-3">Selected Singers:</h5>
+                    {
+                        cart.map(name => <small>
+
+                            <p>{name.name} <img className="m-3" style={{ height: "30px" }} className="cart-img" src={name.img} alt="" /></p>
+                        </small>)
+                    }
                 </div>
                 <div className="card-footer">
                     <div className="text-center">
